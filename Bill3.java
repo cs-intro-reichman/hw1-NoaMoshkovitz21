886923af2378447f90f54caec357b82bb2fca5df
Noa <noamoshkovitz@post.runi.ac.il>
2025-11-02 15:03:04 +0200

    try

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,11 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		int bill  = Integer.parseInt(args[3]);
+		int each  = bill / 3 ; 
+		System.out.println("The guests are: " + name3 +"," + name2 + "," + name1 + "and the bill for each is: " + each);
 	    // Replace this comment with the rest of your code   
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,13 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int mynum = Integer.parseInt(args[0]);
+		
+		int hundreds =  mynum / 100 ;
+		int tens 	 = (mynum / 10 ) % 10 ; 
+		int units    =  mynum % 10 ;
+
+
+		System.out.println(hundreds + " " + "hundreds" + "," + " " + tens + " " + "tens"+ "," + " " + "and " + units +" ones.");
 	}
 }
